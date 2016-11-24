70792c167a6e9e24d4a266023f25aff6497f4215
Chris Walker <clwalker@advent.com>
2016-11-24 01:17:18 -0500

    Adding Jasper property #2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-Handlebars.registerHelper('encode', function (string) {
-  return encodeURIComponent(string);
-});
-
 $(function () {
 	var rentalTemplate = $('#rentals-template').html();
+	var rentalPreviewTemplate = $('#rental-previews-template').html();
 	var compiledRentalTemplate = Handlebars.compile(rentalTemplate);
+	var compiledRentalPreviewTemplate = Handlebars.compile(rentalPreviewTemplate);
 	var compiledRentalHTML = compiledRentalTemplate(json);
+	var compiledRentalPreviewHTML = compiledRentalPreviewTemplate(json);
 
 	$('#rentals').html(compiledRentalHTML);
+	$('#rental-previews').html(compiledRentalPreviewHTML);
 });
--- js/rentals.js
+++ js/rentals.js
@@ -2,7 +2,6 @@ var json = [
   {
     header: '4 beds, 4 baths, 4,095 sqft - Price: $750,000 - Rent: $3500 to $4500 per month. This is market prices.',
     address: '2137 Lake Vilma Drive, Orlando, FL, 32835',
-    contactInfo: 'Email Jasper Burr at deal2017@comcast.net',
     description: [
 			'Executive home in Gated Palma Vista in South West Orlando. Home features all the upgrades, granite counters, new built in refrigerator, marble flooring, 4 large bedrooms, 4 full baths, office, exercise room, huge 2000 sf lanai with screened and salt water pool and spa, outdoor kitchen with Viking appliances, 2 fireplaces, intercom, 5 sets of french doors leading out to pool, tile roof, paver driveway and much more! The Master bedroom has venitian plaster walls, fireplace, large master bath with jetted spa, bidet and large walk in closet. Almost 3/4 of an acre with fenced/walled backyard.',
 			'',
@@ -41,6 +40,13 @@ var json = [
       '2137LakeVilma-3.jpg',
       '2137LakeVilma-4.jpg',
       '2137LakeVilma-5.jpg'
+    ]
+  },
+  {
+    header: 'Price: $250,000',
+    address: '12102 Wynnfield Lakes Cr, Jacksonville, FL, 32246',
+    images: [
+      '12102WynnfieldLakes-1.jpg'
     ]
   }
 ];
